d7e10c47b0193e826ba8bc8910808803f73122e4
RachidCanada <webdsense@gmail.com>
2024-11-16 12:50:33 -0500

    Initial commit for ProductEasy project

---
+++ src/main/java/com/example/ProductEasy/ProductEasyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProductEasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductEasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProductEasy/ProductEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProductEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
